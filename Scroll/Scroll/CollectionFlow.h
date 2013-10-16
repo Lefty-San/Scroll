f873eee7864eb412c80f81847a710191e30abfe5
Eric <guitarprewitt@gmail.com>
2013-10-16 15:28:26 -0600

    creating collection flow layout

---
+++ Scroll/Scroll/CollectionFlow.h
@@ -0,0 +1,13 @@
+//
+//  CollectionFlow.h
+//  Scroll
+//
+//  Created by Eric Prewitt on 10/16/13.
+//  Copyright (c) 2013 Eric Prewitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionFlow : UICollectionViewFlowLayout
+
+@end
---
+++ Scroll/Scroll/CollectionFlow.m
@@ -0,0 +1,13 @@
+//
+//  CollectionFlow.m
+//  Scroll
+//
+//  Created by Eric Prewitt on 10/16/13.
+//  Copyright (c) 2013 Eric Prewitt. All rights reserved.
+//
+
+#import "CollectionFlow.h"
+
+@implementation CollectionFlow
+
+@end
