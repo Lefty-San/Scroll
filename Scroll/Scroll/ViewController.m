2b83b6b7fafd90206ee7ec00b43d0f2f34d5cb16
Eric <guitarprewitt@gmail.com>
2013-10-21 15:37:08 -0600

    stable build, gesture recognizers logs

--- Scroll/Scroll/ViewController.m
+++ Scroll/Scroll/ViewController.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Eric Prewitt. All rights reserved.
 //
 
-/*
+/**
  *
  *  http://skeuo.com/uicollectionview-custom-layout-tutorial
  *  leaving off on step 32
@@ -16,6 +16,9 @@
  *
  *  http://www.mindtreatstudios.com/how-its-made/ios-gesture-recognizer-tips-tricks/
  *  pangesturerecognizer tip
+ *
+ *  /Users/ericprewitt/git/Touches
+ *  touch animation referenced
  *
  */
 
@@ -29,6 +32,7 @@ static NSString * const PhotoCellIdentifier = @"PhotoCell";
 @interface ViewController ()
 
 @property (nonatomic, weak) IBOutlet PhotoAlbumLayout *photoalbumlayout;
+@property (weak, nonatomic) IBOutlet UICollectionViewCell *bodyCollectionCell;
 
 @end
 
@@ -78,7 +82,7 @@ static NSString * const PhotoCellIdentifier = @"PhotoCell";
 }
 
 - (void)setupRecognizers {
-    
+
     UIPanGestureRecognizer* panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(gestureRecognizer:)];
     panRecognizer.minimumNumberOfTouches = 1;
     panRecognizer.delegate = self; // Very important
@@ -89,14 +93,14 @@ static NSString * const PhotoCellIdentifier = @"PhotoCell";
 }
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer {
-    
+
     return YES; // Also, very important.
 }
 
 /**
  Scales up a view slightly which makes the piece slightly larger, as if it is being picked up by the user.
  */
--(void)animateFirstTouchAtPoint:(CGPoint)touchPoint forView:(UIImageView *)theView {
+-(void)animateFirstTouchAtPoint:(CGPoint)touchPoint forView:(UICollectionViewCell *)theView {
 	// Pulse the view by scaling up, then move the view to under the finger.
 	[UIView beginAnimations:nil context:nil];
 	[UIView setAnimationDuration:GROW_ANIMATION_DURATION_SECONDS];
@@ -108,13 +112,15 @@ static NSString * const PhotoCellIdentifier = @"PhotoCell";
  Checks to see which view, or views, the point is in and then sets the center of each moved view to the new postion.
  If views are directly on top of each other, they move together.
  */
-/* TODO add own IBOutlets and substitute in here
--(void)dispatchTouchEvent:(UIView *)theView toPosition:(CGPoint)position
+
+-(void)dispatchTouchEvent:(UICollectionViewCell *)theView toPosition:(CGPoint)position
 {
 	// Check to see which view, or views,  the point is in and then move to that position.
-	if (CGRectContainsPoint([self.firstPieceView frame], position)) {
-		self.firstPieceView.center = position;
+	if (CGRectContainsPoint([self.bodyCollectionCell frame], position)) {
+		self.bodyCollectionCell.center = position;
 	}
+}
+/*
 	if (CGRectContainsPoint([self.secondPieceView frame], position)) {
 		self.secondPieceView.center = position;
 	}
@@ -127,7 +133,7 @@ static NSString * const PhotoCellIdentifier = @"PhotoCell";
 /**
  Scales down the view and moves it to the new position.
  */
--(void)animateView:(UIView *)theView toPosition:(CGPoint)thePosition {
+-(void)animateView:(UICollectionViewCell *)theView toPosition:(CGPoint)thePosition {
 	[UIView beginAnimations:nil context:NULL];
 	[UIView setAnimationDuration:SHRINK_ANIMATION_DURATION_SECONDS];
 	// Set the center to the final postion.
